c15940e672ac293db70c8e81fe1020771550b114
MarcosViniSil <Marcossilv203@gmail.com>
2023-09-24 14:20:00 -0300

    create project

---
+++ src/test/java/br/com/sellbuy/apisellandbuy/ApiSellAndBuyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sellbuy.apisellandbuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSellAndBuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
